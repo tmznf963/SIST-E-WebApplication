ee54661c209ef2043ac9b060cd5c2f32a7ff8f2a
tmznf963 <qhfhd963@naver.com>
2018-10-02 13:53:04 +0900

    update

---
+++ 1002/script/input.js
@@ -0,0 +1,8 @@
+function input(std){
+	std.hakbun = '1101';
+	std.irum = '한송이';
+	std.kor =78;
+	std.eng =87;
+	std.mat =83;
+	std.edp = 78;
+};
---
+++ 1002/script/output.js
@@ -0,0 +1,3 @@
+function output(std){
+	std.toString();
+};
